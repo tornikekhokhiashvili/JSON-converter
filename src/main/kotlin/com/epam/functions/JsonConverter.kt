ff435aa4d6fb79a886f20ac3c32e9fbd258091c5
tornikee <tornikexoxiashvili@gmail.com>
2023-09-04 15:06:33 +0400

    change

--- src/main/kotlin/com/epam/functions/JsonConverter.kt
+++ src/main/kotlin/com/epam/functions/JsonConverter.kt
@@ -8,46 +8,43 @@ object arr {
      * - Any json primitive (number, string, etc.) must be used as is. In some cases numeric values
      * can't be represented without any transformations, in such cases string representation should
      * be used
-     * - If element is instance of [JsonValue], it must be used as is
+     * - If element is an instance of [JsonValue], it must be used as is
      * - Else convert element using [Any]'s [toString] method
-     * For these you should use [toJsonValue] extension of [Any]
+     * For these, you should use [toJsonValue] extension of [Any]
      */
-    operator fun get(
-        vararg elements: Any?
-    ) :JsonArray {
-        val jsonElement=elements.map { element->
-            when{
-                elements==null->JsonNull
-                elements is JsonValue -> element
-                elements is Number -> JsonPrimitive.of(element.toString())
-                element is Char ->JsonPrimitive.of(element.toString())
+    operator fun get(vararg elements: Any?): JsonArray {
+        val jsonElement = elements.map { element ->
+            when (element) {
+                null -> JsonNull
+                is JsonValue -> element
+                is Number -> JsonPrimitive.of(element)
+                is Char -> JsonPrimitive.of(element.toString())
+                is Boolean -> JsonPrimitive.of(element)
                 else -> JsonPrimitive.of(element.toString())
             }
         }
-        return JsonArray.of(jsonElement as List<JsonValue>)
+        return JsonArray.of(jsonElement)
     }
 
     /**
-     * Creates json array from given [elements]. Each element
-     * in the resulting json array must be converted applying the following rules:
-     * - Any json primitive (number, string, etc.) must be used as is. In some cases numeric values
-     * can't be represented without any transformations, in such cases string representation should
+     * Creates a JSON array from given [elements]. Each element
+     * in the resulting JSON array must be converted applying the following rules:
+     * - Any JSON primitive (number, string, etc.) must be used as is. In some cases, numeric values
+     * can't be represented without any transformations, in such cases, string representation should
      * be used
-     * - If element is instance of [JsonValue], it must be used as is
-     * - Else convert element using [Any]'s [toString] method
-     * For these you should use [toJsonValue] extension of [Any]
+     * - If an element is an instance of [JsonValue], it must be used as is
+     * - Else, convert the element using [Any]'s [toString] method
+     * For these, you should use [toJsonValue] extension of [Any]
      */
-    operator fun get(
-        elements: Iterable<Any?>
-    ): JsonArray {
+    operator fun get(elements: Iterable<Any?>): JsonArray {
         val jsonElements = mutableListOf<JsonValue>()
-
         for (element in elements) {
-            val jsonValue: JsonValue = when {
-                element == null -> JsonNull
-                element is JsonValue -> element
-                element is Number -> JsonPrimitive.of(element)
-                element is Char -> JsonPrimitive.of(element.toString())
+            val jsonValue: JsonValue = when (element) {
+                null -> JsonNull
+                is JsonValue -> element
+                is Number -> JsonPrimitive.of(element)
+                is Char -> JsonPrimitive.of(element.toString())
+                is Boolean -> JsonPrimitive.of(element)
                 else -> JsonPrimitive.of(element.toString())
             }
             jsonElements.add(jsonValue)
@@ -57,11 +54,11 @@ object arr {
 }
 
 /**
- * Creates json object using supplied configuration [block]
+ * Creates a JSON object using the supplied configuration [block]
  */
 fun obj(
     block: JsonObjectCreator.() -> Unit
-) :JsonObject{
+): JsonObject {
     val creator = JsonObjectCreator()
     creator.block()
     return creator.toJsonTree()
